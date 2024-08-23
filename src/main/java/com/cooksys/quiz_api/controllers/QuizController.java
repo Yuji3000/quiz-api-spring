806dfabcd5cbb4a2974451159081707c7c9118f6
Yuji Kosakowski <yuji.k.dev@gmail.com>
2024-08-23 10:34:56 -0600

    Add create quiz endpoint with exceptions

--- src/main/java/com/cooksys/quiz_api/controllers/QuizController.java
+++ src/main/java/com/cooksys/quiz_api/controllers/QuizController.java
@@ -2,12 +2,17 @@ package com.cooksys.quiz_api.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cooksys.quiz_api.dtos.QuizRequestDto;
 import com.cooksys.quiz_api.dtos.QuizResponseDto;
 import com.cooksys.quiz_api.services.QuizService;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,6 +28,10 @@ public class QuizController {
     return quizService.getAllQuizzes();
   }
   
-  // TODO: Implement the remaining 6 endpoints from the documentation.
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+	public QuizResponseDto createQuiz(@RequestBody QuizRequestDto quizRequestDto) {
+		return quizService.createQuiz(quizRequestDto);
+	}
 
 }
--- src/main/java/com/cooksys/quiz_api/services/impl/QuizServiceImpl.java
+++ src/main/java/com/cooksys/quiz_api/services/impl/QuizServiceImpl.java
@@ -1,13 +1,18 @@
 package com.cooksys.quiz_api.services.impl;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
+import com.cooksys.quiz_api.dtos.QuizRequestDto;
 import com.cooksys.quiz_api.dtos.QuizResponseDto;
+import com.cooksys.quiz_api.entities.Quiz;
+import com.cooksys.quiz_api.exceptions.BadRequestException;
+import com.cooksys.quiz_api.exceptions.NotFoundException;
 import com.cooksys.quiz_api.mappers.QuizMapper;
 import com.cooksys.quiz_api.repositories.QuizRepository;
 import com.cooksys.quiz_api.services.QuizService;
-
-import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,9 +23,38 @@ public class QuizServiceImpl implements QuizService {
   private final QuizRepository quizRepository;
   private final QuizMapper quizMapper;
 
-  @Override
-  public List<QuizResponseDto> getAllQuizzes() {
-    return quizMapper.entitiesToDtos(quizRepository.findAll());
-  }
+  
+	private void validateQuizRequest(QuizRequestDto quizRequestDto) {
+		if (quizRequestDto.getName() == null ) {
+			throw new BadRequestException("All Fields required for creating a quiz request dto");
+		}
+	}
+
+	private Quiz getQuiz(Long id) {
+		Optional<Quiz> optionalQuiz = quizRepository.findById(id);
+		
+		if (optionalQuiz.isEmpty()) {
+			throw new NotFoundException("No Quiz found with ID:" + id);
+		}
+		return optionalQuiz.get();
+	}
+
+	
+	@Override
+	public QuizResponseDto getQuizById(Long id) {
+		return quizMapper.entityToDto(getQuiz(id));
+	}
+	
+	  @Override
+	  public List<QuizResponseDto> getAllQuizzes() {
+	    return quizMapper.entitiesToDtos(quizRepository.findAll());
+	  }
+
+	@Override
+	public QuizResponseDto createQuiz(QuizRequestDto quizRequestDto) {
+		validateQuizRequest(quizRequestDto);
+		Quiz quizToSave = quizMapper.requestDtoToEntity(quizRequestDto);
+		return quizMapper.entityToDto(quizRepository.saveAndFlush(quizToSave));
+	}
 
 }
